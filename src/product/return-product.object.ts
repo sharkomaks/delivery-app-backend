21685b4a28b5dab9f5e57ab34f7015173052c009
Maksim <sharkomaks@gmail.com>
2025-02-17 15:41:12 +0300

    add product return product object

---
+++ src/product/return-product.object.ts
@@ -0,0 +1,15 @@
+import { Prisma } from '@prisma/client';
+import { returnCategoryObject } from '../category/return-category.object';
+
+export const returnProductObject: Prisma.ProductSelect = {
+	id: true,
+	name: true,
+	description: true,
+	price: true,
+	createdAt: true,
+	slug: true,
+	image: true,
+	category: {
+		select: returnCategoryObject
+	}
+};
