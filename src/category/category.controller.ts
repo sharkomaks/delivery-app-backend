2ab07517a134638efdae1806969ffe81c17771e2
Maksim <sharkomaks@gmail.com>
2025-02-17 15:08:02 +0300

    add category module and controller

---
+++ src/category/category.controller.ts
@@ -0,0 +1,39 @@
+import { Body, Controller, Delete, Get, Param, Post, Put, UsePipes, ValidationPipe } from '@nestjs/common';
+import { CategoryService } from './category.service';
+import { CategoryDto } from './dto/category.dto';
+
+@Controller('categories')
+export class CategoryController {
+	constructor(private readonly categoryService: CategoryService) {}
+
+	@Get()
+	async getAll() {
+		return this.categoryService.getAll();
+	}
+
+	@Get('by-id/:id')
+	async getById(@Param('id') id: string) {
+		return this.categoryService.getById(id);
+	}
+
+	@Get('by-slug/:slug')
+	async getBySlug(@Param('slug') slug: string) {
+		return this.categoryService.getBySlug(slug);
+	}
+
+	@Post()
+	async create() {
+		return this.categoryService.create();
+	}
+
+	@UsePipes(new ValidationPipe())
+	@Put(':id')
+	async update(@Param('id') id: string, @Body() dto: CategoryDto) {
+		return this.categoryService.update(id, dto);
+	}
+
+	@Delete(':id')
+	async delete(@Param('id') id: string) {
+		return this.categoryService.delete(id);
+	}
+}
