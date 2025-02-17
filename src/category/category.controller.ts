a467c794a83cce151b448707276cf534f0c15273
Maksim <sharkomaks@gmail.com>
2025-02-17 15:40:45 +0300

    update category

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Delete, Get, Param, Post, Put, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CategoryDto } from './dto/category.dto';
+import { Auth } from '../auth/decorators/auth.decorator';
 
 @Controller('categories')
 export class CategoryController {
@@ -22,17 +23,20 @@ export class CategoryController {
 	}
 
 	@Post()
+	@Auth()
 	async create() {
 		return this.categoryService.create();
 	}
 
 	@UsePipes(new ValidationPipe())
 	@Put(':id')
+	@Auth()
 	async update(@Param('id') id: string, @Body() dto: CategoryDto) {
 		return this.categoryService.update(id, dto);
 	}
 
 	@Delete(':id')
+	@Auth()
 	async delete(@Param('id') id: string) {
 		return this.categoryService.delete(id);
 	}
