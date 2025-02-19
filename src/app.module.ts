be0bc032abd290b6408768d81027d619710d0c76
Maksim <sharkomaks@gmail.com>
2025-02-19 12:32:18 +0300

    add static uploads

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,21 @@ import { ConfigModule } from '@nestjs/config';
 import { CategoryModule } from './category/category.module';
 import { ProductModule } from './product/product.module';
 import { UserModule } from './user/user.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { path } from 'app-root-path';
 
 @Module({
-	imports: [ConfigModule.forRoot(), AuthModule, CategoryModule, ProductModule, UserModule],
+	imports: [
+		ServeStaticModule.forRoot({
+			rootPath: `${path}/uploads`,
+			serveRoot: '/uploads'
+		}),
+		ConfigModule.forRoot(),
+		AuthModule,
+		CategoryModule,
+		ProductModule,
+		UserModule
+	],
 	controllers: [AppController],
 	providers: [AppService]
 })
